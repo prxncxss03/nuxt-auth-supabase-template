6bd2d91d09f23665e0e2a648d9545d7cf60db8ea
prxncxss03 <pocon.evangeline@gmail.com>
2024-02-07 04:00:24 +0800

    refactor code

--- composables/useRegistration.js
+++ composables/useRegistration.js
@@ -1,38 +1,44 @@
+import { useValidation } from "~/utils/useValidation";
 export const useRegistration = () => {
-  const name = ref('');
-  const email = ref('');
-  const password = ref('');
 
-  const { isValidEmail, isValidPassword, isValidName } = useValidation();
-  const isValidForm = computed(() => isValidEmail(email.value) && isValidPassword(password.value) && isValidName(name.value));
 
-  const register = async () => {
-    if (!isValidForm.value) return;
+    const name = ref('');
+    const email = ref('');
+    const password = ref('');
 
-    try {
-      const response = await $fetch('/api/register', {
-        method: 'POST',
-        body: {
-          name: name.value,
-          email: email.value,
-          password: password.value,
-        },
-      });
-      
-      console.log("register data", response);
-    } catch (error) {
-      console.error("Registration failed", error);
-    }
-  };
+    const { isValidEmail, isValidPassword, isValidName } = useValidation();
+    const isValidForm = computed(() => isValidEmail(email.value) && isValidPassword(password.value) && isValidName(name.value));
+    //
+    console.log("valid email", isValidEmail.value)
+    console.log("valid form", isValidForm.value)
 
-  return {
-    name,
-    email,
-    password,
-    isValidEmail,
-    isValidPassword,
-    isValidName,
-    isValidForm,
-    register,
-  };
+    const register = async () => {
+        if (!isValidForm.value) return;
+
+        try {
+        const response = await $fetch('/api/register', {
+            method: 'POST',
+            body: {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            },
+        });
+        
+        console.log("register data", response);
+        } catch (error) {
+        console.error("Registration failed", error);
+        }
+    };
+
+    return {
+        name,
+        email,
+        password,
+        isValidEmail,
+        isValidPassword,
+        isValidName,
+        isValidForm,
+        register,
+    };
 }
--- pages/register.vue
+++ pages/register.vue
@@ -8,16 +8,16 @@ const { email, password, name, isValidEmail, isValidPassword, isValidName, isVal
 <template>
   <div class="p-4 max-w-md mx-auto">
     <form @submit.prevent="register">
+      <FormField label="Name" :error="!isValidName" errorMessage="Name is too long">
+        <InputText v-model="name" id="name" />
+      </FormField>
+
       <FormField label="Email" :error="!isValidEmail" errorMessage="Invalid Email">
         <InputText v-model="email" id="email" />
       </FormField>
 
       <FormField label="Password" :error="!isValidPassword" errorMessage="Password must be atleast 6 characters">
         <Password v-model="password" id="password" />
-      </FormField>
-
-      <FormField label="Name" :error="!isValidName" errorMessage="Name is too long">
-        <InputText v-model="name" id="name" />
       </FormField>
 
       <Button type="submit" :disabled="!isValidForm" class="w-full">Register</Button>
