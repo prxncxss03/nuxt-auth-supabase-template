e18eda13cb4867e91e2fc6c1d2240c9a4030b372
prxncxss03 <pocon.evangeline@gmail.com>
2024-02-08 23:07:36 +0800

    fix style in regular user dashboard

--- pages/auth/regular/dashboard.vue
+++ pages/auth/regular/dashboard.vue
@@ -18,8 +18,11 @@
 </script>
 
 <template>
-    <div>
-        <p class="text-2xl">Regular User</p>
-        <Button label="Sign Out" @click="signOut"/>
+    <div class="flex font-inter dark:bg-gray-900 justify-center items-center h-screen">
+        <div class="text-center">
+        <h1 class="text-4xl font-bold dark:text-gray-100">Welcome User!</h1>
+        <p class="text-lg dark:text-gray-300">You are now logged in</p>
+        <Button @click="signOut" class="mt-4" label="Logout" />
+        </div>
     </div>
 </template>
