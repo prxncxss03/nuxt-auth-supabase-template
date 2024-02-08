0b065260869853241eafbff6658157cd6282aae2
prxncxss03 <pocon.evangeline@gmail.com>
2024-02-08 23:49:05 +0800

    refactor admin dashboard

--- pages/auth/admin/dashboard.vue
+++ pages/auth/admin/dashboard.vue
@@ -14,44 +14,8 @@
         'auth',
     ]
    })
-    const user = useSupabaseUser()
-    const sessions = ref(null);
-
-    onMounted( () => {
-        fetchSessions();
-        console.log("User from admin: ", user.value);
-       
-    });
-
-    const fetchSessions = async () => {
-        const { data } = await $fetch('/api/admin/getSessions');
-        sessions.value = data;
-    }
     
-    const sessionsData = computed(() => {
-
-        if (sessions.value) {
-            return sessions.value.map((item) => {
-                const userCookie = useCookie('userCookie');
-                const isCurrentUser = userCookie.value === item.user_id;
-                console.log(`user_id: ${item.user_id}, isCurrentUser: ${isCurrentUser}`);
-                return {
-                    user_id: item.user_id,
-                    count: item.count,
-                    badge: isCurrentUser ? 'You' : ''
-                }
-            });
-        }
-        return [];
-    });
-  
-
-    const columns = [
-    {field: 'user_id', header: 'User ID'},
-    {field: 'count', header: 'Logins'},
-    {field: 'badge', header: ''}
-
-];
+   const { sessionsData, columns } = useFetchSessions()
 
 </script>
 
