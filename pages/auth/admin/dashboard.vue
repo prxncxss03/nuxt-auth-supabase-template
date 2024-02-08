7b5389ad8a5d34c2c5a4b23bfb4b79e65b4cca36
prxncxss03 <pocon.evangeline@gmail.com>
2024-02-08 23:25:17 +0800

    fix style in admin dashboard

--- pages/auth/admin/dashboard.vue
+++ pages/auth/admin/dashboard.vue
@@ -57,9 +57,17 @@
 </script>
 
 <template>
-    <div>
-        <div class="card">
-            <DataTable :value="sessionsData" tableStyle="min-width: 50rem"> 
+    <div class="flex flex-col font-inter dark:bg-gray-900  h-screen items-center ">
+    <div class="flex self-end">
+        <Button @click="signOut" class="mt-4 mr-2" label="Logout" />
+    </div>
+     <div class="flex flex-col justify-center items-center h-full">
+        <div class="text-center">
+        <h1 class="text-4xl font-bold dark:text-gray-100">Welcome Admin!</h1>
+        <p class="text-lg dark:text-gray-300 mb-2">You are now logged in</p>
+        </div>
+        <div class="card ">
+            <DataTable  :value="sessionsData" tableStyle="min-width: 20rem"> 
                 <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header">
                     <template v-if="col.field === 'badge'" #body="slotProps">
                         <Tag v-if="slotProps.data.badge" value="You" rounded></Tag>
@@ -67,6 +75,8 @@
                 </Column>
             </DataTable>
         </div>
-        <Button label="Sign Out" @click="signOut"/>
+        
     </div>
+</div>
+    
 </template>
